0e343e7ad3836c8066e2a23dba911c7a4310f1ef
Jeremy Roussel <jeremy.roussel13@gmail.com>
2020-07-09 14:11:20 -0500

    Working Recipe Images, Card text needs layout work

--- JS/recipes.js
+++ JS/recipes.js
@@ -15,7 +15,28 @@ $(() => {
             .done((results) =>{
                 console.log(results);
                 for (let i = 0; i<3; i++) {
-                    console.log(results.hits[i].recipe);
+                    console.log(results.hits[i].recipe)
+                    
+                    let $cardDiv = $('<div>', {
+                        'class': 'col flex-column h-25 boxFont m-1',
+                    });
+
+                    let $cardIMG = $('<img>', {
+                        'class': "foodImage rounded-lg",
+                        'src': results.hits[i].recipe.image,
+                        'alt': results.hits[i].recipe.label
+
+                    });
+
+                    let $cardPar = $('<p', {
+                        'class': "p-1 text-center font font-weight-bold",
+                        'text': results.hits[i].recipe.label
+                    })
+
+                    $cardDiv.append($cardIMG)
+                    $cardDiv.append($cardPar)
+                    $recipeBox.append($cardDiv)
+                      
                 }
             })
     }
