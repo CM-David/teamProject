0c3c2954342db1b1a02b8e855766a098190cc0e6
Jeremy Roussel <jeremy.roussel13@gmail.com>
2020-07-10 14:59:31 -0500

    Modal button assigned, needs updates to fields, in particular the API call to fetch nutrition data

--- JS/recipes.js
+++ JS/recipes.js
@@ -28,13 +28,21 @@ $(() => {
 
                     });
 
-                    let $cardPar = $('<p>', {
-                        'class': "p-1 text-center font font-weight-bold",
+                    // let $cardPar = $('<p>', {
+                    //     'class': "p-1 text-center font font-weight-bold",
+                    //     'text': results.hits[i].recipe.label
+                    // })
+                    let $cardButton = $('<button>', {
+                        type: "button",
+                        class: "btn btn-primary",
+                        'data-toggle': "modal",
+                        'data-target': "#exampleModal",
+                        'data-whatever': results.hits[i].recipe.label,
                         'text': results.hits[i].recipe.label
                     })
 
                     $cardDiv.append($cardIMG)
-                    $cardDiv.append($cardPar)
+                    $cardDiv.append($cardButton)
                     $recipeBox.append($cardDiv)
                       
                 }
@@ -47,3 +55,13 @@ $(() => {
 
 
 })
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+  })
